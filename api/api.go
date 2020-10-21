4d96c0f1bc5ab23e67acf82c5d82239ad4562dbd
laojianzi <laojianzi1994@gmail.com>
2020-10-21 11:16:16 +0800

    repo: supports lint and fix lint error

--- api/api.go
+++ api/api.go
@@ -14,35 +14,35 @@ type App struct {
 	once   sync.Once
 }
 
-var app = new(App)
+var fiberApp = new(App)
 
 // Server return a api.App
 func Server() *App {
-	app.once.Do(func() {
-		app.server = fiber.New(fiber.Config{
+	fiberApp.once.Do(func() {
+		fiberApp.server = fiber.New(fiber.Config{
 			ServerHeader:         "MDClubGo",
 			ReadTimeout:          5 * time.Second,
 			WriteTimeout:         10 * time.Second,
 			CompressedFileSuffix: ".mdclubgo.gz",
 		})
 
-		app.route()
+		fiberApp.route()
 	})
 
-	return app
+	return fiberApp
 }
 
 // Add add handler to api server
-func (a *App) Add(method string, path string, handlers ...fiber.Handler) fiber.Router {
-	return a.server.Add(method, path, handlers...)
+func (app *App) Add(method string, path string, handlers ...fiber.Handler) fiber.Router {
+	return app.server.Add(method, path, handlers...)
 }
 
 // Start listen api server
-func (a *App) Start(addr string) error {
-	return a.server.Listen(addr)
+func (app *App) Start(addr string) error {
+	return app.server.Listen(addr)
 }
 
 // Test send test request to api server
-func (a *App) Test(req *http.Request, msTimeout ...int) (resp *http.Response, err error) {
-	return a.server.Test(req, msTimeout...)
+func (app *App) Test(req *http.Request, msTimeout ...int) (resp *http.Response, err error) {
+	return app.server.Test(req, msTimeout...)
 }
